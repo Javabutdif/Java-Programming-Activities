184cda2fb2d9d3a94f47daed9f708e4c25fffa67
Murasame123 <116171287+Javabutdif@users.noreply.github.com>
2023-05-26 09:00:55 +0800

    Asterisk Box

--- Main.java
+++ Main.java
@@ -1,47 +1,25 @@
-import java.util.*;
-import javax.swing.*;
-
+import java.util.Scanner;
 public class Main {
-	
 	public static void main(String[] args) {
-		
-		while(true) {
-		int choice = Integer.parseInt(JOptionPane.showInputDialog("[1] Add Enrolees, [2] Delete Enrolees"));
-		
-		if(choice ==1) {
-			displayAdd();
-		}
-		else if(choice ==2)
-			deleteStudent();
-		else if(choice == 3)
-			show();
-		}
-
-	}
-	private static void displayAdd() {
-		String lastname = "";;
-		String firstname = "";
-		String middlename = "";
-		int age = 0;
-		String course = "";
-		lastname = JOptionPane.showInputDialog("Input Last Name");
-		firstname = JOptionPane.showInputDialog("Input First Name");
-		middlename = JOptionPane.showInputDialog("Input Middle Name");
-		age = Integer.parseInt(JOptionPane.showInputDialog("Input Age"));
-		course = JOptionPane.showInputDialog("Input Course");
-		
-		Student stud = new Student(lastname, firstname, middlename, age, course);
-		stud.addStudent();
-
-	}
-	private static void deleteStudent() {
-		String lastname = JOptionPane.showInputDialog("Input Last Name to Delete");
-		new Student(lastname);
+		Scanner console = new Scanner(System.in);
+		System.out.print("Enter number:");
+		int num = console.nextInt();
+			for(int i = 0; i <= num; i++) {
+				
+				for(int j=0; j <= num; j++) {
+					if(i==0 || i == num|| j==0 || j==num) {
+					System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
+					
+				}
+				System.out.println();
+			
+			}
+			System.out.println();
 		
 	}
-	private static void show() {
-		Student stud1 = new Student();
-		stud1.showStudent();
-	}
-
+		
 }
