ce01b2defdae5322d6ca59fe7fa98f00e9ba14a7
Murasame123 <116171287+Javabutdif@users.noreply.github.com>
2023-05-26 09:06:33 +0800

    Stack

--- Main.java
+++ Main.java
@@ -1,25 +1,39 @@
-import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
+
 	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in);
-		System.out.print("Enter number:");
-		int num = console.nextInt();
-			for(int i = 0; i <= num; i++) {
-				
-				for(int j=0; j <= num; j++) {
-					if(i==0 || i == num|| j==0 || j==num) {
-					System.out.print("*");
-					}
-					else {
-						System.out.print(" ");
-					}
-					
-				}
-				System.out.println();
-			
-			}
-			System.out.println();
+		
+		Stack <String> stack = new Stack<String>();
+		
+		Name honda = new Name();
+		Name ford = new Name();
+		Name toyota= new Name();
+		
+		honda.name = "SpaceX";
+		honda.brand = "Honda";
+		honda.year = 2022;
+		
+		toyota.name = "Beach";
+		toyota.brand = "Toyota";
+		toyota.year = 2021;
+		
+		ford.name = "JupiterX";
+		ford.brand = "Ford";
+		ford.year = 2023;
+		
+		//Stack kay Last in First OUT
+		stack.push(honda.getName());//1st
+		stack.push(ford.getName());//2nd
+		stack.push(toyota.getName());//3rd
+		
+		for(int i  = 0; i < stack.capacity(); i++) {
+			System.out.println(stack.pop());
+			System.out.println("===================");
+		}
+		
 		
 	}
-		
+	
+
 }
